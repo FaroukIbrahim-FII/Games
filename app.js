cad0a94bd42db61e08c21536cd7d40740d93c021
Fdoubleye <>
2021-06-16 12:26:51 +0000

    adding Javascript file

---
+++ app.js
@@ -0,0 +1,30 @@
+var name = prompt("what is your name?")
+
+alert("welcome to Games Wiki" + name)
+
+var Age = prompt("How are you?")
+
+if(Age >= 18){
+  alert("That means you can see and play every game in our website")
+}
+else{
+  alert("You cannot see or play every game we have in the website. You are welcome to try what suits you")
+
+}
+
+var FavGame = prompt("what is your favorite game? GTA V, Fallout 4 or Animal Crossing?")
+
+if(FavGame == "GTA V"){
+  alert("Great! We have what you are asking for.")
+}
+else if(FavGame == "Fallout 4"){
+  alert("Great! We have what you are asking for.")
+}
+else if(FavGame == "Animal Crossing"){
+  alert("Great! We have what you are asking for.")
+}
+else{
+  alert("Sorry, we don't have what you asked for, but feel free to see what we have.")
+}
+
+
